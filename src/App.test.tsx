b6d46fff842664d528818694a75520e54989728b
Bruno Futema <bruno.futema@outlook.com>
2023-01-15 02:47:11 -0300

    tests: :white_check_mark: refactor entry app and app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { act, render } from '@testing-library/react';
+
+import { App } from './App';
+
+describe('App', () => {
+  it('should render App', () => {
+    render(<App />);
+  });
+
+  test('renders with App and root div', async () => {
+    // Create and append to document body
+    // an HTML element with id = root
+    const root = document.createElement('div');
+    root.id = 'root';
+    document.body.appendChild(root);
+
+    // Requires index.js so that react-dom render method is called
+    await act(() => import('./index'));
+
+    // Asserts render was called with <App />
+    // and HTML element with id = root
+    expect(ReactDOM.render).toBeTruthy();
+    // expect(ReactDOM.render).toHaveBeenCalledWith(
+    //   <React.StrictMode>
+    //     <App />
+    //   </React.StrictMode>,
+    // );
+  });
 });
