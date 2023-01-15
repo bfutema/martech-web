460f881c93417d63e162a41f3768f7ab408ed703
Bruno Futema <bruno.futema@outlook.com>
2023-01-15 02:50:46 -0300

    test: :white_check_mark: add router config

---
+++ src/routes/routes.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import { Router } from '.';
+
+describe('Router', () => {
+  it('should render Router', () => {
+    render(<Router />);
+  });
+});
