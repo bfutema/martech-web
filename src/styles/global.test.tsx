0ec9b97e95f6b73d4eaea9cdc796b6e0c025bad6
Bruno Futema <bruno.futema@outlook.com>
2023-01-15 02:53:44 -0300

    test: :white_check_mark: add global styles

---
+++ src/styles/global.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import { GlobalStyles } from './global';
+
+describe('GlobalStyles', () => {
+  it('should render GlobalStyles', () => {
+    render(<GlobalStyles />);
+  });
+});
