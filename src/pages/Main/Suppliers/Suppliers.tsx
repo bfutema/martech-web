54c88eb4eeb4d0e4007f0db2ecef5a2c18c63380
Bruno Futema <bruno.futema@outlook.com>
2023-01-15 03:08:45 -0300

    feat: :sparkles: add Suppliers page

---
+++ src/pages/Main/Suppliers/Suppliers.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './Suppliers.styles';
+
+export const Suppliers: React.FC = () => {
+  return (
+    <Container>
+      <h1>Suppliers</h1>
+    </Container>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { Suppliers } from '../pages/Main';
+
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div />} />
+        <Route path="/" element={<Suppliers />} />
       </Routes>
     </BrowserRouter>
   );
