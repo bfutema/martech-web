d630821f28dca3c385e38159c5a0bb1273e6eeb1
Bruno Futema <bruno.futema@outlook.com>
2023-01-16 01:44:43 -0300

    style: :art: configure global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { GlobalStyles } from '@styles/global';
+
 import { Router } from './routes';
 
 export const App: React.FC = () => {
   return (
     <>
       <Router />
+      <GlobalStyles />
     </>
   );
 };
