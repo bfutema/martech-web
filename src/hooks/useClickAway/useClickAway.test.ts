b487924e962c475dc9932d33cff3ddca39607ed6
Bruno Futema <bruno.futema@outlook.com>
2023-01-16 02:05:49 -0300

    test: :white_check_mark: fix useClickAway

--- src/hooks/useClickAway/useClickAway.test.ts
+++
@@ -1,9 +0,0 @@
-import { renderHook } from '@testing-library/react';
-
-import { useClickAway } from './useClickAway';
-
-describe('useClickAway', () => {
-  it('should be able to use useClickAway', () => {
-    renderHook(() => useClickAway(jest.fn(), jest.fn(), { enabled: true }));
-  });
-});
---
+++ src/hooks/useClickAway/useClickAway.test.tsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+
+import { fireEvent, render, renderHook, screen } from '@testing-library/react';
+
+import { useClickAway } from './useClickAway';
+
+function Component() {
+  const ref = useRef(null);
+
+  useClickAway(ref, jest.fn(), { enabled: true });
+
+  return (
+    <div ref={ref}>
+      <span>useClickAway</span>
+    </div>
+  );
+}
+
+describe('useClickAway', () => {
+  it('should be able to use hook useClickAway', () => {
+    renderHook(() => useClickAway(jest.fn(), jest.fn(), { enabled: true }));
+
+    render(
+      <div data-testid="outside">
+        <Component />
+      </div>,
+    );
+
+    const outside = screen.getByTestId('outside');
+
+    fireEvent.pointerDown(outside);
+  });
+});
