806793740428653b870a8e0f92eed681569d4ab0
Bruno Futema <bruno.futema@outlook.com>
2023-01-19 01:47:04 -0300

    refactor: remove app test comments

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,23 +11,13 @@ describe('App', () => {
   });
 
   it('renders with App and root div', async () => {
-    // Create and append to document body
-    // an HTML element with id = root
     const root = document.createElement('div');
     root.id = 'root';
     document.body.appendChild(root);
 
-    // Requires index.js so that react-dom render method is called
     await act(() => import('./index'));
 
-    // Asserts render was called with <App />
-    // and HTML element with id = root
     expect(ReactDOM.render).toBeTruthy();
-    // expect(ReactDOM.render).toHaveBeenCalledWith(
-    //   <React.StrictMode>
-    //     <App />
-    //   </React.StrictMode>,
-    // );
   });
 
   it('reportWebVitals', () => reportWebVitals(() => jest.fn()));
