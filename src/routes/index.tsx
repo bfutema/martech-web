18bd47a4c509e68faa630e740e2ee7b50ecc93e9
Bruno Futema <bruno.futema@outlook.com>
2023-01-19 03:33:55 -0300

    feat: :sparkles: apply MainLayout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { MainLayout } from '@layouts/MainLayout';
+
 import { Suppliers } from '../pages/Main';
 
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Suppliers />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Suppliers />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
