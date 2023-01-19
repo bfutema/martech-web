6845ffdc35ff3b7ea4a91761ed4bcfe9340e3a2b
Bruno Futema <bruno.futema@outlook.com>
2023-01-19 03:30:36 -0300

    feat: :sparkles: add menu icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
+export { ReactComponent as ArrowDown } from './arrow-down.svg';
 export { ReactComponent as CalendarLine } from './calendar-line.svg';
 export { ReactComponent as ChevronLeft } from './chevron-left.svg';
 export { ReactComponent as ChevronRight } from './chevron-right.svg';
-export { ReactComponent as ArrowDown } from './arrow-down.svg';
+export { ReactComponent as Menu } from './menu.svg';
