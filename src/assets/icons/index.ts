540a880f0ceae1a453c8eb821ff120121015165f
Bruno Futema <bruno.futema@outlook.com>
2023-01-19 01:41:32 -0300

    refactor: add arrow down asset

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as CalendarLine } from './calendar-line.svg';
 export { ReactComponent as ChevronLeft } from './chevron-left.svg';
 export { ReactComponent as ChevronRight } from './chevron-right.svg';
+export { ReactComponent as ArrowDown } from './arrow-down.svg';
